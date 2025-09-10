6daa6d00d6561db6068fb602032ae0b1294d4164
Johnny Herrera <jmjaramillo95@gmail.com>
2025-09-09 21:51:49 -0500

    Agregar archivos de proyecto.

---
+++ colecciones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colecciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+
+            /*
+            numeros.Add(5);
+            numeros.Add(6);
+            numeros.Add(7);
+            */
+
+            int[] listaNumeros = new int[] {3, 6, 8, 2, 4}; 
+
+            for(int i = 0; i < 5; i++)
+            {
+                numeros.Add(listaNumeros[i]);
+            }
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+        }
+    }
+}
