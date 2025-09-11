fc99574cd6f831955a2d4a9b14915f6a782d5526
Johnny Herrera <jmjaramillo95@gmail.com>
2025-09-10 23:15:23 -0500

    collections

--- colecciones/Program.cs
+++ colecciones/Program.cs
@@ -18,6 +18,7 @@ namespace colecciones
             numeros.Add(7);
             */
 
+            /*
             int[] listaNumeros = new int[] {3, 6, 8, 2, 4}; 
 
             for(int i = 0; i < 5; i++)
@@ -28,6 +29,44 @@ namespace colecciones
             for(int i = 0; i < 5; i++)
             {
                 Console.WriteLine(numeros[i]);
+            }
+            */
+
+            /*
+            Console.WriteLine("¿Cuantos elementos quieres introducir?");
+
+            int elem = Int32.Parse(Console.ReadLine());
+
+            for(int  i = 0; i < elem; i++)
+            {
+                numeros.Add(Int32.Parse(Console.ReadLine()));
+            }
+
+
+            for (int i = 0; i < elem; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+            */
+
+            Console.WriteLine("Introduce elementos en l colección (0 para salir)");
+
+            int elem = 1;
+
+            while (elem != 0)
+            {
+                elem = Int32.Parse(Console.ReadLine());
+
+                numeros.Add(elem);
+            }
+
+            numeros.RemoveAt(numeros.Count-1);
+
+            Console.WriteLine("Elemento introducidos: ");
+
+            foreach (var item in numeros)
+            {
+                Console.WriteLine(item);
             }
         }
     }
